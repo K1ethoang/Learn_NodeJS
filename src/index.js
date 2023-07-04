9b253be116b1d83c09ab6930c3a7689daf46d603
Kiet Hoang <kiethoang101@gmail.com>
2023-07-04 23:31:17 +0700

    Add: 04/07/23

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// HTTP logger
+app.use(morgan("dev"));
+
+// Template Engine
+app.engine("hbs", handlebars.engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
